1e351af5f1c8a511aab08c7a7f194f27b88a84e0
Saurabh Mahindre <saurabh.mahindre@paytm.com>
2018-05-05 18:34:13 +0530

    kaggle docker

---
+++ docker.sh
@@ -0,0 +1,13 @@
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get install docker-ce
+
+sudo docker pull kaggle/python
+
+sudo docker run --rm -it     -v $PWD:/tmp/working -w=/tmp/working     -p 8888:8
+888 kaggle/python jupyter notebook     --no-browser --ip="0.0.0.0"     --notebook-dir=/tmp/working     --allow-root
