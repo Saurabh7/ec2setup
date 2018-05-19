ff62164f753785c44d0d568434f66cbf2b386e91
Saurabh Mahindre <saurabh.mahindre@paytm.com>
2018-05-19 19:21:29 +0530

    add basic notebook

--- docker.sh
+++ docker.sh
@@ -1,5 +1,7 @@
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
+sudo apt-get install unzip
+
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
@@ -9,5 +11,4 @@ sudo apt-get install docker-ce
 
 sudo docker pull kaggle/python
 
-sudo docker run --rm -it     -v $PWD:/tmp/working -w=/tmp/working     -p 8888:8
-888 kaggle/python jupyter notebook     --no-browser --ip="0.0.0.0"     --notebook-dir=/tmp/working     --allow-root
+sudo docker run --rm -it     -v $PWD:/tmp/working -w=/tmp/working     -p 8888:8888 kaggle/python jupyter notebook     --no-browser --ip="0.0.0.0"     --notebook-dir=/tmp/working     --allow-root
