d165a7bde81fe7c6da457bd96dbc75f0c54d42a4
Saurabh Mahindre <saurabh.mahindre@gmail.com>
2018-05-29 17:48:40 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,14 @@
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-sudo apt-get install unzip htop
-sudo apt-get install python-pip
-sudo pip install kaggle
-
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
 sudo apt-get update
+sudo apt-get install unzip htop
+sudo apt-get install python-pip
+pip install kaggle
 
 sudo apt-get install docker-ce
 
