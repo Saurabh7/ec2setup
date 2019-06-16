35cff56c84ffd74b0ff143bd156214320f10cce8
Saurabh Mahindre <saurabh.mahindre@gmail.com>
2019-06-16 10:55:14 +0530

    Add CUDA setup

--- setup.sh
+++ setup.sh
@@ -14,4 +14,10 @@ sudo apt-get install docker-ce
 
 sudo docker pull kaggle/python
 
+wget https://developer.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda-repo-ubuntu1804-10-1-local-10.1.168-418.67_1.0-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1804-10-1-local-10.1.168-418.67_1.0-1_amd64.deb
+sudo apt-key add /var/cuda-repo-10-1-local-10.1.168-418.67/7fa2af80.pub
+sudo apt-get update
+sudo apt-get install cuda
+
 sudo docker run --rm -it     -v $PWD:/tmp/working -w=/tmp/working     -p 8888:8888 kaggle/python jupyter notebook     --no-browser --ip="0.0.0.0"     --notebook-dir=/tmp/working     --allow-root
